bfe1e0c18c286e0b9fa68ba3bfa0561af237d1b6
Conanjun <1526840124@qq.com>
2018-06-08 22:02:13 +0800

    modify popup.js and popup.hmtl

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,9 @@
 $(function() {
     $('#search_button').click(function () {
+        //获取当前页面的url和content
+        //解析指定后缀的资源，webrequest加载然后分析匹配指定规则的内容
+        //显示结果
         alert('search button clicked');
     })
 });
+
