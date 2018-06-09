82e80e8181fe2c7938fba8dff5a05386808079df
Conanjun <1526840124@qq.com>
2018-06-09 09:59:10 +0800

    add front html

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,16 @@
 $(function () {
+    //规则按钮事件
+    $('#add_suffix_button').click(function (e) {
+        $('#file_suffix').append('<div><input type="text" name="suffix" value=""/><a href="#" class="removeclass"><input type=\'button\' value=\'del\'></a></div>');
+    });
+    $('#add_rule_regexp_button').click(function (e) {
+        $('#rule_regexp').append('<div><input type="text" name="rule_regexp" value=""/><a href="#" class="removeclass"><input type=\'button\' value=\'del\'></a></div>');
+    });
+    $('body').on('click',".removeclass",function (e) {
+       $(this).parent('div').remove();
+    });
+
+    //设置搜索按钮事件
     $('#search_button').click(function () {
         var current_url = '';
         var current_html = '';
@@ -10,19 +22,24 @@ $(function () {
         //获取当前页面的url和content
         chrome.tabs.getSelected(null, function (tab) {
             //alert(tab.url);
-            console.log(tab.url)
+            //console.log(tab.url)
             current_url = tab.url;
             chrome.tabs.sendMessage(tab.id, {action: "get_content"}, function (response) {
                 //console.log(response);
                 current_html = response;
-                //解析指定后缀的资源，webrequest加载然后分析匹配指定规则的内容
+                //解析指定后缀的资源，webrequest加载然后分析匹配指定规则的内容,由于这可能花费的时间比较多，需要交给background.js
                 //显示结果
             });
         })
-    })
+    });
 });
 
-//解析html中的指定后缀的资源，根据指定规则匹配敏感信息
+//根据后缀规则获取html中指定后缀的字典 {'js':['1.js','2.js'],'php':['1.php','2.php']}
+function get_suffix_list(html) {
+    //TODO
+}
+
+//根据指定规则匹配敏感信息
 function regexp_analyze(content, rule_regexp) {
     //TODO
 }
