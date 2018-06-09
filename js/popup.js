f065aafc625a4ea049c3cf1d412411912e0048bc
iamnobady <739424849@qq.com>
2018-06-09 13:16:00 +0800

    basic page and get input data

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,30 @@
 $(function () {
     //规则按钮事件
-    $('#add_suffix_button').click(function (e) {
+    /*$('#add_suffix_button').click(function (e) {
         $('#file_suffix').append('<div><input type="text" name="suffix" value=""/><a href="#" class="removeclass"><input type=\'button\' value=\'del\'></a></div>');
     });
     $('#add_rule_regexp_button').click(function (e) {
         $('#rule_regexp').append('<div><input type="text" name="rule_regexp" value=""/><a href="#" class="removeclass"><input type=\'button\' value=\'del\'></a></div>');
-    });
+    });*/
     $('body').on('click',".removeclass",function (e) {
-       $(this).parent('div').remove();
+    	var a=$(this).attr('class');
+       $(this).parent('td').parent('tr').remove();
     });
-
+    $('body').on('click',".add_suffix",function (e) {
+    	var add_target=$(this);
+        add_suffix(add_target);
+    })
+    $('body').on('click',".add_regexp",function (e) {
+    	var add_target=$(this);
+        add_regexp(add_target);
+    })
+     $('body').on('click',".add_execlude",function (e) {
+    	var add_target=$(this);
+        add_execlude(add_target);
+    })
+    $('#search_button').click(function () {
+        search();
+    })
     //设置搜索按钮事件
     $('#search_button').click(function () {
         var current_url = '';
@@ -29,6 +44,7 @@ $(function () {
                 current_html = response;
                 //解析指定后缀的资源，webrequest加载然后分析匹配指定规则的内容,由于这可能花费的时间比较多，需要交给background.js
                 //显示结果
+                
             });
         })
     });
@@ -48,3 +64,53 @@ function test() {
     console.log('test');
 }
 
+function add_suffix(e){
+        //alert(1);
+        var add='<tr><td><input type="text" name="suffix" value="" class="suffix"/></td><td class="focus"><a href="#" id="add_suffix_button" class="add_suffix show">&nbsp;+</a><a href="#" class="removeclass hide">&nbsp;x</a></td></tr>';
+        e.addClass('hide');
+        e.parent('td').find('.removeclass').removeClass('hide');
+        $('.focus').removeClass('focus');
+        $('.suffix_table').append(add);
+        //alert(e);
+}
+
+function add_regexp(e){
+        //alert(1);
+        var add='<tr><td><input type="text" name="rule_regexp" value=""/></td><td class="focus_regexp"><a href="#" id="add_regexp_button" class="add_regexp show">&nbsp;+</a><a href="#" class="removeclass hide">&nbsp;x</a></td></tr>';
+        e.addClass('hide');
+        e.parent('td').find('.removeclass').removeClass('hide');
+        $('.focus_regexp').removeClass('focus_regexp');
+        $('.regexp_table').append(add);
+        //alert(e);
+}
+function add_execlude(e){
+        //alert(1);
+        var add='<tr><td><input type="text" name="suffix" value=""/></td><td class="focus_execlude"><a href="#" class="add_execlude show">&nbsp;+</a><a href="#" class="removeclass hide">&nbsp;x</a></td></tr>';
+        e.addClass('hide');
+        e.parent('td').find('.removeclass').removeClass('hide');
+        $('.focus_execlude').removeClass('focus_execlude');
+        $('.execlude_table').append(add);
+        //alert(e);
+}
+
+var suffix = new Array();
+var regexp = new Array();
+var execlude = new Array();
+function search(){
+	var suffix_num = $('input[name="suffix"]').length;
+	var regexp_num = $('input[name="rule_regexp"]').length;
+	var execlude_num = $('input[name="execlude"]').length;
+	var i;
+	for(i=0;i<suffix_num;i++){
+		suffix[i] = $('input[name="suffix"]:eq('+[i]+')').val();
+		console.log(suffix[i]);
+	}
+	for(i=0;i<regexp_num;i++){
+		regexp[i] = $('input[name="rule_regexp"]:eq('+[i]+')').val();
+		console.log(regexp[i]);
+	}
+	for(i=0;i<execlude_num;i++){
+		execlude[i] = $('input[name="execlude"]:eq('+[i]+')').val();
+		console.log(execlude[i]);
+	}
+}
