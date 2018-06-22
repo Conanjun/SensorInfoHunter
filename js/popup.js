51c4d7d7ca497e3b9a79a02551ba56397d897fdb
iamnobady <739424849@qq.com>
2018-06-22 17:17:01 +0800

    basic page

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,6 @@
 $(function () {
-    $('#get_url').click(function () {
-        chrome.tabs.getSelected(null, function (tab) {
-            $('#show_url').text(tab.url);
-        })
+    chrome.tabs.getSelected(null, function (tab) {
+        $('#url_span').text(tab.url);
     });
     //设置搜索按钮事件
     $('#search_button').click(function () {
@@ -19,9 +17,18 @@ $(function () {
             //console.log(tab.url)
             current_url = tab.url;
             chrome.tabs.sendMessage(tab.id, {action: "get_content"}, function (response) {
-
                 console.log('into sendMessage callback')
                 current_html = response;
+                if(typeof current_html == "undefined"){
+                    console.log("refresh");
+                    $(".warning").css("display","inline-block");
+                    $("#search_button").css("left","161px");
+                    setTimeout(display_refresh,1000);
+                }
+                else{
+                    
+                    console.log(current_html);
+                }
                 //解析指定后缀的资源，webrequest加载然后分析匹配指定规则的内容,由于这可能花费的时间比较多，需要交给background.js
                 //显示结果
 
@@ -29,6 +36,11 @@ $(function () {
         })
     });
 });
+
+function display_refresh(){
+    $(".warning").css("display","none");
+    $("#search_button").css("left","320px");
+}
 
 //根据后缀规则获取html中指定后缀的字典 {'js':['1.js','2.js'],'php':['1.php','2.php']}
 function get_suffix_list(html) {
